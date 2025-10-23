29941a2aa238195c5b7cf172d9ccc7fe815e4509
girlando-junior <girlandojunior209@gmail.com>
2025-10-23 00:03:30 -0300

    finalizando header

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -1,8 +1,21 @@
+import { Package } from "lucide-react";
+
+import { User } from "lucide-react";
+import { MoreVertical } from "lucide-react";
+
 export default function Header() {
   return (
-    <div className="bg-[var(--color-primary)] p-[16px] flex items-center justify-between">
-      <div className="flex items-center p-4 gap-2 text-sm text-gray-600">
-        <h1 className="font-bold">STORAGE</h1>
+    <div className="bg-[var(--color-primary)] p-[16px] flex items-center">
+      <div className="flex items-center justify-between w-full">
+        <div className="flex items-center gap-4">
+          <Package size={40} />
+          <h1 className="font-bold">STORAGE</h1>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <MoreVertical size={40} />
+          <User size={40} />
+        </div>
       </div>
     </div>
   );
